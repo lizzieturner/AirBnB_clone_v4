e13d4ddc5fab750bc6cf5ac2c5850933039e6b1f
Lizzie Turner <hello@lizzieturner.com>
2018-09-08 21:25:50 +0000

    Create 3-hbnb

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,45 @@
+// listen for changes on each INPUT checkbox tag
+let checked = {};
+$(document).ready(function () {
+  $('input:checkbox').change(function () {
+    if ($(this).is(':checked')) {
+      checked[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete checked[$(this).data('id')];
+    }
+    $('div.amenities h4').html(function () {
+      let amenities = [];
+      Object.keys(checked).forEach(function (key) {
+        amenities.push(checked[key]);
+      });
+      if (amenities.length === 0) {
+        return ('&nbsp');
+      }
+      return (amenities.join(', '));
+    });
+  });
+});
+
+// Check API status and update status marker in the header
+let url = 'http://0.0.0.0:5001/api/v1/status/';
+$.get(url, function (data, status) {
+  console.log(data);
+  if (data.status === 'OK') {
+    $('div#api_status').addClass('available');
+  } else {
+    $('div#api_status').removeClass('available');
+  }
+});
+
+// implement place search function
+$.ajax({
+  type: 'POST',
+  url: 'http://0.0.0.0:5001/api/v1/places_search/',
+  contentType: 'application/json',
+  data: {},
+  success: function (data) {
+    for (let currentPlace of data) {
+      $('.places ').append('<article> <h2>' + currentPlace.name + '</h2> </article>');
+    }
+  }
+});
