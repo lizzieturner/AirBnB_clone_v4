244d9104175091b0f9c8a513948db708f53b5c30
Lizzie Turner <hello@lizzieturner.com>
2018-09-08 22:38:19 +0000

    Load places from front end, rather than from backend using Ajax

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -36,10 +36,10 @@ $.ajax({
   type: 'POST',
   url: 'http://0.0.0.0:5001/api/v1/places_search/',
   contentType: 'application/json',
-  data: {},
+  data: '{}',
   success: function (data) {
     for (let currentPlace of data) {
-      $('.places ').append('<article> <h2>' + currentPlace.name + '</h2> </article>');
+      $('.places').append('<article> <div class="title"> <h2>' + currentPlace.name + '</h2><div class="price_by_night">' + currentPlace.price_by_night + '</div></div> <div class="information"> <div class="max_guest"> <i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + currentPlace.max_guest + ' Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + currentPlace.number_rooms + ' Bedrooms </div> <div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + currentPlace.number_bathrooms + ' Bathroom  </div></div> <div class="user"></div><div class="description">' + currentPlace.description + '</div></article>');
     }
   }
 });
