977fc61fccf0af92fcf531f3ca45475e1a7bc546
Derek <dhklangs@gmail.com>
2018-09-08 18:04:27 +0000

    Fix text wrapping so that ellipsis shows up if the text does not fit

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,4 +1,5 @@
 // listen for changes on each INPUT checkbox tag
+
 let checked = {};
 $(document).ready(function () {
   $('input:checkbox').change(function () {
@@ -7,5 +8,15 @@ $(document).ready(function () {
     } else {
       delete checked[$(this).data('id')];
     }
+    $('div.amenities h4').html(function () {
+      let amenities = [];
+      Object.keys(checked).forEach(function (key) {
+        amenities.push(checked[key]);
+      });
+      if (amenities.length === 0) {
+        return ('&nbsp');
+      }
+      return (amenities.join(', '));
+    });
   });
 });
