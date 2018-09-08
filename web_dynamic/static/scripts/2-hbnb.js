6e2898e53bc7792574da28a5a20f70a65ac1e76c
Derek <dhklangs@gmail.com>
2018-09-08 19:10:57 +0000

    Copy existing checkbox functionality to the new script

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,3 +1,5 @@
+// Check API status and update status marker in the header
+
 let url = 'http://0.0.0.0:5001/api/v1/status/';
 $.get(url, function (data, status) {
   if (status === 'OK') {
@@ -5,4 +7,27 @@ $.get(url, function (data, status) {
   } else {
     $('div#api_status').removeClass('available');
   };
+});
+
+// listen for changes on each INPUT checkbox tag
+
+let checked = {};
+$(document).ready(function () {
+  $('input:checkbox').change(function () {
+    if ($(this).is(':checked')) {
+      checked[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete checked[$(this).data('id')];
+    }
+    $('div.amenities h4').html(function () {
+      let amenities = [];
+      Object.keys(checked).forEach(function (key) {
+        amenities.push(checked[key]);
+      });
+      if (amenities.length === 0) {
+        return ('&nbsp');
+      }
+      return (amenities.join(', '));
+    });
+  });
 });
