63789b4ff4ad7a6107bf0e67c362d459e65a0c49
Derek <dhklangs@gmail.com>
2018-09-08 19:55:35 +0000

    Add functionality to change color of API indicator if API is successfully running

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -2,7 +2,8 @@
 
 let url = 'http://0.0.0.0:5001/api/v1/status/';
 $.get(url, function (data, status) {
-  if (status === 'OK') {
+  console.log(data);
+  if (data.status === 'OK') {
     $('div#api_status').addClass('available');
   } else {
     $('div#api_status').removeClass('available');
